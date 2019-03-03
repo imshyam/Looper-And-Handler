1a393b393cff1d2c0a73b0a90278da48d68c3760
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-03-03 18:12:17 +0800

    add 1 more handler

--- app/src/main/java/com/shyam/looperandhandler/HandlerWorker.java
+++ app/src/main/java/com/shyam/looperandhandler/HandlerWorker.java
@@ -7,16 +7,23 @@ public class HandlerWorker extends HandlerThread {
 
 
     private Handler handler;
+    private Handler handler2;
     private static final String TAG = "HandlerWorker";
 
     HandlerWorker() {
         super(TAG);
         start();
         handler = new Handler(getLooper());
+        handler2 = new Handler(getLooper());
     }
 
     HandlerWorker execute(Runnable task) {
         handler.post(task);
+        return this;
+    }
+
+    HandlerWorker executeViaOther(Runnable task) {
+        handler2.post(task);
         return this;
     }
 
