7cbb8ac8c163608261a117c238787388ef59cce9
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-03-03 16:58:34 +0800

    run tasks from main

--- app/src/main/java/com/shyam/looperandhandler/MainActivity.java
+++ app/src/main/java/com/shyam/looperandhandler/MainActivity.java
@@ -1,21 +1,83 @@
 package com.shyam.looperandhandler;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 
 import com.shyam.looperandhandler.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyWorker worker;
+    private ActivityMainBinding binding;
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            binding.setText((String) msg.obj);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         binding.setText("This is bound");
 
+        worker = new MyWorker();
+
+        worker.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = new Message();
+                msg.obj = "Task 1 Complete";
+                handler.sendMessage(msg);
+            }
+        })
+        .execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = new Message();
+                msg.obj = "Task 2 Complete";
+                handler.sendMessage(msg);
+            }
+        })
+        .execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = new Message();
+                msg.obj = "Task 3 Complete";
+                handler.sendMessage(msg);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        worker.quit();
     }
 }
